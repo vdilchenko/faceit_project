f9b6f78f35a6025d4d98f621a3f6276f9a9cbc09
Vyacheslav <vdilchenko@gmail.com>
2021-04-11 13:43:49 +0600

    made asynchronous changes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect
 import plotly
 import plotly.graph_objs as go
-from faceit_stats import get_stats
+from faceit_stats import player_stat
 import json
+import asyncio 
 
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ def post_form():
     if request.method == "POST":
         nickname = request.form['nickname']
         limit = request.form['limit'] if request.form['limit'] is not None else 20
-        stats = get_stats(nickname, limit)
+        stats = loop.run_until_complete(player_stat(nickname, limit))
         bar = create_plot(stats)
         return render_template("index.html", plot=bar)
     return render_template("index.html")
@@ -31,14 +32,14 @@ def create_plot(stats):
     averager = window_mean()
     data = [
         go.Scatter(
-            x=[x for x in range(len(stats['Kills']))],
-            y=[kills/deaths for kills, deaths in zip(stats['Kills'][::-1], stats['Deaths'][::-1])],
+            x=[x for x in range(len(stats))],
+            y=[k/d for k, d in stats[::-1]],
             hovertext='KD',
             name='KD per match'
         ),
         go.Scatter(
-            x=[x for x in range(len(stats['Kills']))],
-            y=[averager(k, d) for k, d in zip(stats['Kills'], stats['Deaths'])],
+            x=[x for x in range(len(stats))],
+            y=[averager(k, d) for k, d in stats[::-1]],
             hovertext='KD',
             name='AVG KD'
         )
@@ -49,4 +50,6 @@ def create_plot(stats):
 
 
 if __name__ == "__main__":
-    app.run()
+    loop = asyncio.get_event_loop()
+    app.run(debug=True)
+    loop.close()
--- faceit_stats.py
+++ faceit_stats.py
@@ -1,8 +1,7 @@
 import requests
 import json
-import collections
-from concurrent.futures import ThreadPoolExecutor
-
+from aiohttp import ClientSession
+import asyncio
 
 with open('key.api', 'r') as f:
     API_KEY = f.readline().strip()
@@ -14,23 +13,34 @@ def request(url, **kwargs):
     return json.loads(requests.get(DATA_API_URL + url.format(**kwargs), headers=headers).content)
 
 
-def get_stats(nickname, limit, game_id='csgo'):
-    player_id = request('/players?nickname={nickname}', nickname=nickname)['player_id']
-    history = request('/players/{player_id}/history?game={game_id}&offset=0&limit={limit}',
+def get_history(nickname, limit, game_id='csgo'):
+        player_id = request('/players?nickname={nickname}', nickname=nickname)['player_id']
+        history = request('/players/{player_id}/history?game={game_id}&offset=0&limit={limit}',
                       player_id=player_id, limit=limit, game_id=game_id)['items']
-    with ThreadPoolExecutor(4) as executor:
-        stats = list(executor.map(calc_stats, list(history), [player_id]*len(history)))
-    kills = [val[0] for val in stats]
-    deaths = [val[1] for val in stats]
-    return {'Kills': kills, 'Deaths': deaths}
+        return history, player_id
 
 
-def calc_stats(history, player_id):
-    match_id = history['match_id']
-    data = request('/matches/{match_id}/stats', match_id=match_id)
+async def get_stats(match_id, player_id):
+    async with ClientSession() as session:
+        resp = await session.get(
+            DATA_API_URL + f'/matches/{match_id}/stats',
+            headers=headers)
+        data = await resp.json()
     for team in data['rounds'][0]['teams']:
         for player in team['players']:
             if player_id == player['player_id']:
                 kills = int(player['player_stats']['Kills'])
                 deaths = int(player['player_stats']['Deaths'])
     return kills, deaths
+
+
+def get_coros(nickname, limit):
+    history, player_id = get_history(nickname, limit)
+    to_do = [get_stats(match['match_id'], player_id) for match in history]
+    return to_do
+
+
+async def player_stat(nickname, limit):
+    to_do = get_coros(nickname, limit)
+    res = await asyncio.gather(*to_do)
+    return res
